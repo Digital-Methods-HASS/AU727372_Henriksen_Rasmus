3a9cf21f41f79d36de79d1dadcc68b669699b7a0
RasmusBHenriksen <144114831+RasmusBHenriksen@users.noreply.github.com>
2023-10-03 12:23:17 +0200

    Add files via upload

---
+++ Aflvering_til_w39_Opgave1.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+read_csv("data/dansk_kongerække_dk - Ark1.csv")
+
+Kongerække <- read_csv("data/dansk_kongerække_dk - Ark1.csv")
+
+Kongerække <- Kongerække %>%
+  mutate(Regeringstid = Regeringslut - Regeringsstart)
+
+colnames(Kongerække) 
+
+Kongerække %>%
+  ggplot(aes(y = Regeringstid, x = Regeringsstart))+
+         geom_point()+
+  geom_line()+
+  geom_smooth()+
+  labs(x = "Tid",
+       Y = "Regeringstid")+
+  theme_minimal() 
+ 
+ggsave("Kongerækken_visualisering.png", units = "cm", width = 20, height = 40)              
+
